b525b19e7c6b9081cdd418e131fb3b301f08411b
kevin.zhou <910871311@outlook.com>
2020-01-17 17:27:05 +0800

    Update batch3.sh

--- XcodeBuildDemo/batch3.sh
+++ XcodeBuildDemo/batch3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-CONFIG_ROOT_PATH="/Users/housl/Desktop/XcodeBuildDemo/"
+CONFIG_ROOT_PATH="$PWD"
 cd $CONFIG_ROOT_PATH
-distDir="/Users/housl/Desktop/package"
+distDir="$PWD/package"
 rm -rdf "$distDir"
 #桌面创建文件夹，存放生成的包
 mkdir "$distDir"
@@ -9,16 +9,16 @@ mkdir "$distDir"
 for line in $(cat channel.txt)
 do
 #每次打新包前清空
-rm -rf "/Users/housl/Desktop/XcodeBuildDemo/build/"
+rm -rf "$PWD/build/"
 #XcodeBuildDemo  需要打包的target  （当前demo值）
 xcodebuild -target XcodeBuildDemo clean
 ipafilename=`echo $line|cut -f1 -d':'`
 echo "ipafilename=$ipafilename"
 targetName=`echo ${ipafilename}`
 #修改工程plist值 CFrom需要改变的key值，后面跟需要修改的plist文件路径
-/usr/libexec/PlistBuddy -c "set :CFrom ${targetName}" /Users/housl/Desktop/XcodeBuildDemo/XcodeBuildDemo/proChannel.plist
+/usr/libexec/PlistBuddy -c "set :CFrom ${targetName}" $PWD/proChannel.plist
 #打包 XcodeBuildDemo 需要打包的target
 xcodebuild -target XcodeBuildDemo -configuration Distribution -sdk iphoneos build
 #生成ipa -v 打包生成的app路径
-xcrun -sdk iphoneos PackageApplication -v "/Users/housl/Desktop/XcodeBuildDemo/build/Release-iphoneos/XcodeBuildDemo.app" -o "/Users/housl/Desktop/package/${targetName}.ipa"
+xcrun -sdk iphoneos PackageApplication -v "$PWD/build/Release-iphoneos/XcodeBuildDemo.app" -o "$PWD/package/${targetName}.ipa"
 done
